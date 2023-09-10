cb7189fcf6943eeda9290a784a9e49d79d2a6aae
Mulukal-Gemechu <mulukalgemechu@gmail.com>
2023-09-10 12:15:23 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,14 +6,12 @@
  *  * main - Entry point
  *   *
  *    * Return: Always 0 (Success)
- **/
+ *     **/
 
 int main(void)
 {
 char low;
 for (low = 'a'; low <= 'z'; low++)
-putchar(low);
-for (low = 'A'; low <= 'Z'; low++)
 putchar(low);
 putchar('\n');
 return (0);
