fc2cdd096af1c8ee32af582b5a5c95a7898aadbc
Mulukal-Gemechu <mulukalgemechu@gmail.com>
2023-09-14 09:29:53 -0700

    print_to_98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  * print_to_98 - function to printnumbers 0 to 98.
+ *   * @n: integer
+ *    * Return: 0
+ *     **/
+void print_to_98(int n)
+{
+if (n <= 98)
+{
+while (n <= 98)
+{
+printf("%d", n);
+if (n < 98)
+printf(", ");
+n++;
+}
+}
+else
+{
+while (n >= 98)
+{
+printf("%d", n);
+if (n > 98)
+printf(", ");
+n--;
+}}
+printf("\n");
+}
