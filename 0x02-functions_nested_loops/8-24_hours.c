042bd7814af99c82dceb59bbe8efd47915e05a13
Mulukal-Gemechu <mulukalgemechu@gmail.com>
2023-09-14 09:24:00 -0700

    hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *  * jack_bauer - Check Holberton
+ *   * Description: function prints every minute in one day
+ *    * Return: Nothing
+ *     **/
+void jack_bauer(void)
+{
+int x = 0;
+int j = 0;
+while (x <= 23)
+{
+j = 0;
+while (j <= 59)
+{
+_putchar((x / 10) + '0');
+_putchar((x % 10) + '0');
+_putchar(':');
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
+_putchar('\n');
+j += 1;
+}
+x += 1;
+}
+}
